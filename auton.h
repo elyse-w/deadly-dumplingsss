31be02519be89f7ae3a360ce3a6df929f8dbf78a
elyse-w <elyse.m.wada@gmail.com>
2026-02-18 08:15:01 -0800

    auton.h

---
+++ auton.h
@@ -0,0 +1,15 @@
+#include "api.h"
+#include "main.h"
+#include "okapi/api.hpp"
+#include "pros/api_legacy.h"
+#ifndef AUTONH
+#define AUTONH
+
+extern void run_autonomous();
+
+
+#endif
+
+
+
+// //cole was here. you better win this year!
